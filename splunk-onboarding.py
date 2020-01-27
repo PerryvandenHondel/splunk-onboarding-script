5bb2171f07727d54e8786c561949c5f508fd22a2
Perry van den Hondel <perry.vandenhondel@gmail.com>
2020-01-27 16:30:00 +0100

    Aded new defs

--- splunk-onboarding.py
+++ splunk-onboarding.py
@@ -9,24 +9,57 @@ Source: https://pynative.com/python-mysql-tutorial/
 
 
 
+from pathlib import Path
 import mysql.connector
 from mysql.connector import Error
 
 
 
-def SelectDeploymentAppsToBeCreated(environment):
+def CreateDirectoryNameForDeploymentApp(number):
     '''
-    Select only the records form the view that needs to be created for the environment
+    Create a deployment app directory name in format 'da-999999'
+
+    number: number to use in the deployment app directory, number = 1 returns da-000001
+    '''
+    t = str(number)
+    t = 'da-' + t.zfill(6) 
+    return t
+
+
+
+def CreateOneDeploymentApp(logFileId, directoryCreate, pathToMonitor, sourceType):
+    '''
+    Create a new deployment app in the required directory
+
+    logFileId: Unique log file ID. Every log file has a unqiue ID
+    directoryCreate: Starting directory where to create the new inputs.conf
+    pathToMonitor: Which log file to monitor in the inputs.conf
+    sourceType: Use this source type in the inputs stanza
+    '''
+    print('CreateOneDeploymentApp()')
+
+    directoryFull = directoryCreate + '/' + CreateDirectoryNameForDeploymentApp(logFileId) +'/local/'
+    print(directoryFull)
+    Path(directoryFull).mkdir(parents=True, exist_ok=True)
+
+
+
+def ProcessDeploymentApps(environment):
+    '''
+    Select only the records from the view that needs to be created for the environment
 
     environment: Code of the Splunk environment thats needs to be created.
     '''
-    query = "SELECT * FROM view_create_new_da WHERE lgf_env_code='" + environment + "' AND lgf_created=0"
+    query = "SELECT * FROM view_create_deployment_app WHERE cda_env_code='" + environment + "' AND cda_is_created=0"
     cursor = connection.cursor()
     cursor.execute(query)
     records = cursor.fetchall()
-    print('SelectDeploymentAppsToBeCreated(): record found to process')
+    print('CreateDeploymentApp(): record found to process')
     for row in records:
-        print(row[0], '\t', row[1], '\t', row[2], '\t', row[3], '\t', row[4], '\t', row[5], '\t', row[6])
+        print(row[0], '\t', row[1], '\t', row[2], '\t', row[3], '\t', row[4], '\t', row[5], '\t', row[6], '\t', row[7],  '\t', row[8])
+        CreateOneDeploymentApp(row[3], row[5], row[6], row[7])
+
+    
 
 
 
@@ -49,18 +82,21 @@ def main():
     except Error as e:
         print("Error while connecting to MySQL", e)
     
-    sql_select_Query = "select * from environment_env"
-    cursor = connection.cursor()
-    cursor.execute(sql_select_Query)
-    records = cursor.fetchall()
-    print("Total number of rows in environment_env is: ", cursor.rowcount)
+    ##sql_select_Query = "select * from environment_env"
+    ##cursor = connection.cursor()
+    ##cursor.execute(sql_select_Query)
+    #3records = cursor.fetchall()
+    ##print("Total number of rows in environment_env is: ", cursor.rowcount)
 
-    print('Code==== Description==================== RCD==================== RLU================')
-    for row in records:
-        print(row[0], '\t', row[1], '\t',  row[2], '\t', row[3])
+    ##print('Code==== Description==================== RCD==================== RLU================')
+    ##for row in records:
+    ##    print(row[0], '\t', row[1], '\t',  row[2], '\t', row[3])
 
 
-    SelectDeploymentAppsToBeCreated('VOY')
+
+    ProcessDeploymentApps('VOY')
+
+    print(CreateDirectoryNameForDeploymentApp(43))
 
 
     if (connection.is_connected()):
